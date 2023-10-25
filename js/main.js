bf713337f4289d46d520b839087c92e3da62e7e9
RIMAS, DANIELLA YVETTE D <daniellayvetterimas@gmail.com>
2023-10-26 06:05:20 +0800

    final app feature

--- js/main.js
+++ js/main.js
@@ -1,54 +1,121 @@
 (function ($) {
   "use strict";
 
-  new WOW().init();
-    
-    // Sticky Navbar
-    $(function () {
-        var navbar = $('.navbar');
-        $(window).scroll(function () {
-        if ($(window).scrollTop() <= 40) {
-            navbar.removeClass('navbar-scroll');
-        } else {
-            navbar.addClass('navbar-scroll');
-        }
-    });
+// Initialize the image, text, and subtitle arrays
+let designArr = [
+  {
+    'Feature 1': ['assets/img/feature-1.png', 'Feature 1', 'Feature 1 Subtitle'],
+    'Feature 2': ['assets/img/feature-2.png', 'Feature 2', 'Feature 2 Subtitle'],
+    'Feature 3': ['assets/img/feature-3.png', 'Feature 3', 'Feature 3 Subtitle'],
+    'Feature 4': ['assets/img/feature-4.png', 'Feature 4', 'Feature 4 Subtitle'],
+    'Feature 5': ['assets/img/feature-5.png', 'Feature 5', 'Feature 5 Subtitle'],
+    'Feature 6': ['assets/img/feature-6.png', 'Feature 6', 'Feature 6 Subtitle'],
+    'Feature 7': ['assets/img/feature-7.png', 'Feature 7', 'Feature 7 Subtitle'],
+    'Feature 8': ['assets/img/feature-8.png', 'Feature 8', 'Feature 8 Subtitle'],
+    'Feature 9': ['assets/img/feature-9.png', 'Feature 9', 'Feature 9 Subtitle'],
+    'Feature 10': ['assets/img/feature-10.png', 'Feature 10', 'Feature 10 Subtitle'],
+  }
+];
+
+// Initialize the image, text, and subtitle elements
+let changeImage = $("#changeImage");
+let changeTitle = $("#changeTitle");
+let changeSubtitle = $("#changeSubtitle");
+
+new WOW().init();
+  
+// Sticky Navbar
+$(function () {
+  var navbar = $('.navbar');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() <= 40) {
+      navbar.removeClass('navbar-scroll');
+    } else {
+      navbar.addClass('navbar-scroll');
+    }
+  });
+});
+
+$(document).ready(function () {
+  // Select the navbar element
+  const navbar = $('.navbar');
+
+  // Function to toggle the 'navbar-scroll' class based on scroll position
+  function toggleNavbarBackground() {
+    if ($(window).scrollTop() > 0) {
+      navbar.addClass('navbar-scroll');
+    } else {
+      navbar.removeClass('navbar-scroll');
+    }
+  }
+
+  // Initial check when the page loads
+  toggleNavbarBackground();
+
+  // Check for scroll events
+  $(window).scroll(toggleNavbarBackground);
+});
+
+
+// Smooth scrolling on the navbar links
+$(document).ready(function () {
+  $('a.nav-link').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+
+      const hash = this.hash;
+      const offset = 70; // Adjust this value if needed
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - offset
+      }, 800); // Adjust the animation duration as needed
+    }
   });
 
-    $(document).ready(function () {
-        // Select the navbar element
-        const navbar = $('.navbar');
-      
-        // Function to toggle the 'navbar-scroll' class based on scroll position
-        function toggleNavbarBackground() {
-          if ($(window).scrollTop() > 0) {
-            navbar.addClass('navbar-scroll');
-          } else {
-            navbar.removeClass('navbar-scroll');
-          }
-        }
-      
-        // Initial check when the page loads
-        toggleNavbarBackground();
-      
-        // Check for scroll events
-        $(window).scroll(toggleNavbarBackground);
-      });
-      
+  // Screenshot carousel
+  $(".screenshot-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    loop: true,
+    dots: true,
+    items: 1,
+    margin: 10,
+  });
 
-    // Smooth scrolling on the navbar links
-    $(document).ready(function () {
-        $('a.nav-link').on('click', function (event) {
-          if (this.hash !== '') {
-            event.preventDefault();
-      
-            const hash = this.hash;
-            const offset = 70; // Adjust this value if needed
-      
-            $('html, body').animate({
-              scrollTop: $(hash).offset().top - offset
-            }, 800); // Adjust the animation duration as needed
-          }
-        });
-      });
+  // Initialize the image, text, and subtitle elements
+  changeImage.attr('src', designArr[0]['Feature 1'][0]);
+  changeTitle.text(designArr[0]['Feature 1'][1]);
+  changeSubtitle.text(designArr[0]['Feature 1'][2]);
+
+  // Add class to each dot
+  let dot = $('.owl-dot');
+
+  dot.each(function () {
+    let index = $(this).index() + 1;
+    $(this).addClass('Feature ' + index);
+  });
+
+  // Change image, title, and subtitle when dot is clicked
+  dot.on('click', function () {
+    let index = $(this).index() + 1;
+    $(".screenshot-carousel").trigger('to.owl.carousel', [index, 1000]);
+
+    changeImageText(index);
+  });
+
+  // Change image, title, and subtitle when carousel is changed
+  function changeImageText(index) {
+    changeImage.attr('src', designArr[0]['Feature ' + index][0]);
+    changeTitle.text(designArr[0]['Feature ' + index][1]);
+    changeSubtitle.text(designArr[0]['Feature ' + index][2]);
+  }
+
+  let activeDot = $('.owl-dot.active');
+  $(".screenshot-carousel").on('changed.owl.carousel', function (event) {
+    activeDot = $('.owl-dot.active');
+    let index = activeDot.index() + 1;
+    changeImageText(index);
+  });
+});
+
 })(jQuery);
